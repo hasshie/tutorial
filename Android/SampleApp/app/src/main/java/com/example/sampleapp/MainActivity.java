1143721373f0fae6247ab366faa4a455901503b2
sevenstar\hasshie <airman.hm@gmail.com>
2023-04-21 00:11:37 +0900

    ListView サンプル

--- Android/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.java
+++ Android/SampleApp/app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.sampleapp;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+//implements AdapterView.OnItemSelectedListener
 public class MainActivity extends AppCompatActivity  {
     AlertDialog dialog;
     AlertDialog.Builder builder;
@@ -16,22 +19,9 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //        builder = new AlertDialog.Builder(getActivity())
-//                .setTitle("データの削除")
-//                .setMessage("データを削除します。")
-//                .setPositiveButton(R.string.app_name, new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // User clicked OK button
-//                    }
-//                })
-//                .setNegativeButton(R.string.hello_first_fragment, new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // User cancelled the dialog
-//                    }
-//                });
-//        // ここにデータを削除するメソッドを記述
-////        }
 
+        Log.d("### MainActivity" , String.format("STEP: %s", "onCreate"));
+        Toast.makeText(this, "MainActivity onCreate",Toast.LENGTH_LONG).show();;
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,20 +34,57 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
-// Set other dialog properties
-//...
 
-// Create the AlertDialog
-//        dialog = builder.create();
+        ListView lv = findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item , new String[] {
+                "aaa",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",
+                "aaa",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB",  "BBB"
+        });
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.d("### MainActivity.onItemClick" , String.format("STEP: %s", i));
+            }
+        });
+        lv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.d("### MainActivity.onItemSelected" , String.format("STEP: %s", i));
+//                Toast.makeText(this, "MainActivity:" + i,Toast.LENGTH_LONG).show();;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("### MainActivity.onNothingSelected" , String.format("getSelectedItemPosition: %s",adapterView.getSelectedItemPosition()));
+
+//                Toast.makeText(this, "MainActivity:onNothingSelected",Toast.LENGTH_LONG).show();;
+
+            }
+        });
+
     }
 
-//
-//    public void onClickBtn() {
+//    @Override
+//    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//        Log.d("### " , String.format("STEP: %s", i));
+//        Toast.makeText(this, "MainActivity:" + i,Toast.LENGTH_LONG).show();;
 //    }
 
 //    @Override
-//    public void onClick(View view) {
-//        Snackbar.make(view, "aaa", Snackbar.LENGTH_LONG).show();
+//    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//        Log.d("### MainActivity.onItemSelected" , String.format("STEP: %s", i));
+//        Toast.makeText(this, "MainActivity:" + i,Toast.LENGTH_LONG).show();;
+//    }
 //
+//    @Override
+//    public void onNothingSelected(AdapterView<?> adapterView) {
+//        Log.d("### MainActivity.onNothingSelected" , String.format("STEP: %s",adapterView.toString()));
+//
+//        Toast.makeText(this, "MainActivity:onNothingSelected",Toast.LENGTH_LONG).show();;
 //    }
 }
